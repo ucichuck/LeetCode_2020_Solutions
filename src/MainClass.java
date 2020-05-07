e45886f72139dd2338f7aff841f22eb0cb9579f3
Chuck <ucichuck@gmail.com>
2020-05-06 22:38:06 -0700

    Solution about "Tree Iterator"

--- src/MainClass.java
+++ src/MainClass.java
@@ -18,11 +18,40 @@ public class MainClass {
 //        Q560 so = new Q560();
 //        int res = so.subarraySum(new int[] {1,1,3,1,1}, 5);
 
-        LeftMostColumnIndexOfOne so = new LeftMostColumnIndexOfOne();
-//        int res = so.getLeftMostColumnIndex(new int[][] {{0, 0, 0, 1}, {0, 0, 1, 1}, {0, 1, 1, 1}, {0, 0, 0, 1}});
-//        int res = so.getLeftMostColumnIndex(new int[][] {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}});
-        int res = so.getLeftMostColumnIndex(new int[][] {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}});
+//        LeftMostColumnIndexOfOne so = new LeftMostColumnIndexOfOne();
+////        int res = so.getLeftMostColumnIndex(new int[][] {{0, 0, 0, 1}, {0, 0, 1, 1}, {0, 1, 1, 1}, {0, 0, 0, 1}});
+////        int res = so.getLeftMostColumnIndex(new int[][] {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}});
+//        int res = so.getLeftMostColumnIndex(new int[][] {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}});
 
-        System.out.println(res);
+        TreeNode head = new TreeNode(7);
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(15);
+        TreeNode node3 = new TreeNode(9);
+        TreeNode node4 = new TreeNode(20);
+        head.left = node1;
+        head.right=node2;
+        node2.left = node3;
+        node2.right = node4;
+
+        TreeIterator it = new TreeIterator(head);
+        print("it.hasNext()", it.hasNext()); // true
+        print("it.next()", it.next()); // 3
+        print("it.next()", it.next()); // 7
+        print("it.next()", it.next()); // 9
+        print("it.next()", it.next()); // 15
+        print("it.hasPrev()", it.hasPrev()); // true
+        print("it.prev()", it.prev()); // 9
+        print("it.hasPrev()", it.hasPrev()); // false because we can only move 1 step back
+        print("it.next()", it.next()); // 15
+        print("it.next()", it.next()); // 20
+        print("it.hasNext()", it.hasNext()); // false
+        print("it.hasPrev()", it.hasPrev()); // true
+        print("it.prev()", it.prev()); // 15
+        print("it.hasNext()", it.hasNext()); // true
+        print("it.next()", it.next()); // 20
+    }
+
+    private static void print(String des, Object res) {
+        System.out.println(des + " : " + res);
     }
 }
