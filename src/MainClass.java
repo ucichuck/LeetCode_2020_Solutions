a71e8d28c48d9acf5d9a6d458c2211f0135bc2e4
Chuck <ucichuck@gmail.com>
2020-05-07 23:11:00 -0700

    Solution about "Counting sort"

--- src/MainClass.java
+++ src/MainClass.java
@@ -23,32 +23,39 @@ public class MainClass {
 ////        int res = so.getLeftMostColumnIndex(new int[][] {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}});
 //        int res = so.getLeftMostColumnIndex(new int[][] {{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}});
 
-        TreeNode head = new TreeNode(7);
-        TreeNode node1 = new TreeNode(3);
-        TreeNode node2 = new TreeNode(15);
-        TreeNode node3 = new TreeNode(9);
-        TreeNode node4 = new TreeNode(20);
-        head.left = node1;
-        head.right=node2;
-        node2.left = node3;
-        node2.right = node4;
+//        TreeNode head = new TreeNode(7);
+//        TreeNode node1 = new TreeNode(3);
+//        TreeNode node2 = new TreeNode(15);
+//        TreeNode node3 = new TreeNode(9);
+//        TreeNode node4 = new TreeNode(20);
+//        head.left = node1;
+//        head.right=node2;
+//        node2.left = node3;
+//        node2.right = node4;
+//
+//        TreeIterator it = new TreeIterator(head);
+//        print("it.hasNext()", it.hasNext()); // true
+//        print("it.next()", it.next()); // 3
+//        print("it.next()", it.next()); // 7
+//        print("it.next()", it.next()); // 9
+//        print("it.next()", it.next()); // 15
+//        print("it.hasPrev()", it.hasPrev()); // true
+//        print("it.prev()", it.prev()); // 9
+//        print("it.hasPrev()", it.hasPrev()); // false because we can only move 1 step back
+//        print("it.next()", it.next()); // 15
+//        print("it.next()", it.next()); // 20
+//        print("it.hasNext()", it.hasNext()); // false
+//        print("it.hasPrev()", it.hasPrev()); // true
+//        print("it.prev()", it.prev()); // 15
+//        print("it.hasNext()", it.hasNext()); // true
+//        print("it.next()", it.next()); // 20
 
-        TreeIterator it = new TreeIterator(head);
-        print("it.hasNext()", it.hasNext()); // true
-        print("it.next()", it.next()); // 3
-        print("it.next()", it.next()); // 7
-        print("it.next()", it.next()); // 9
-        print("it.next()", it.next()); // 15
-        print("it.hasPrev()", it.hasPrev()); // true
-        print("it.prev()", it.prev()); // 9
-        print("it.hasPrev()", it.hasPrev()); // false because we can only move 1 step back
-        print("it.next()", it.next()); // 15
-        print("it.next()", it.next()); // 20
-        print("it.hasNext()", it.hasNext()); // false
-        print("it.hasPrev()", it.hasPrev()); // true
-        print("it.prev()", it.prev()); // 15
-        print("it.hasNext()", it.hasNext()); // true
-        print("it.next()", it.next()); // 20
+        CountSort countSort = new CountSort();
+        int[] nums = new int[] {10,20,21,30,15,19,16,22,22,20,21,19,30};
+        countSort.countSort(nums, 10, 30);
+        for (int i : nums) {
+            print("num", i);
+        }
     }
 
     private static void print(String des, Object res) {
