ae0fc8054fee0bca021fae02772511609797e640
ucichuck <ucichuck@gmail.com>
2020-05-09 17:47:16 -0700

    Create Q20.java

---
+++ src/Q20.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char cur : s.toCharArray()) {
+            if (cur == '(' || cur == '{' || cur == '[') {
+                st.push(cur);
+            } else if (cur == ')' || cur == '}' || cur == ']') {
+                if (st.isEmpty()) {
+                    return false;
+                } else {
+                    char pre = st.pop();
+                    if (cur == ')' && pre != '(') {
+                        return false;
+                    } else if (cur == '}' && pre != '{') {
+                        return false;
+                    } else if (cur == ']' && pre != '[') {
+                        return false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
