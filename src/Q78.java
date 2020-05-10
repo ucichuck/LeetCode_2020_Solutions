871b717e4c0dd1eae7bd593735bf76549923ff9b
ucichuck <ucichuck@gmail.com>
2020-05-09 17:50:04 -0700

    Create Q78.java

---
+++ src/Q78.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        if (nums.length == 0) {
+            return res;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> tmp = new ArrayList<List<Integer>>();
+            for (int j = 0; j < res.size(); j++) {
+                List<Integer> listTmp = new ArrayList<Integer>();
+                listTmp.addAll(res.get(j));
+                tmp.add(listTmp);
+            }
+            int cur = nums[i];
+            
+            for (List<Integer> list : tmp) {
+                list.add(cur);
+                res.add(list);
+            }
+        }
+        
+        return res;
+    }
+}
