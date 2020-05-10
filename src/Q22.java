ec26217f475eb445095eb4b3e562b579b86c8c03
ucichuck <ucichuck@gmail.com>
2020-05-09 17:48:32 -0700

    Create Q22.java

---
+++ src/Q22.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        if (n <= 0) {
+            res.add("");
+            return res;
+        }
+        
+        generatePa(res, "", 0, 0, n);
+        return res;
+    }
+    
+    private void generatePa(List<String> res, String cur, int left, int right, int n) {
+        if (left == n && right == n) {
+            res.add(cur);
+            return;
+        }
+        
+        if (left < n) {
+             generatePa(res, cur + '(', left + 1, right, n);
+        }
+        
+        if (right < left) {
+             generatePa(res, cur + ')', left, right + 1, n);
+        }
+    }
+}
