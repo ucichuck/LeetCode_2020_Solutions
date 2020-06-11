2602c007bc4e9af14ab5d892dbba914f7960cbb3
Chuck <ucichuck@gmail.com>
2020-06-10 22:02:55 -0700

    Solution about "703. Kth Largest Element in a Stream"

---
+++ src/Q703.java
@@ -0,0 +1,33 @@
+import java.util.PriorityQueue;
+
+public class Q703 {
+    PriorityQueue<Integer> queue;
+    int k;
+
+    public Q703(int k, int[] nums) {
+        queue = new PriorityQueue<>((o1, o2)->{return o1 - o2;});
+        this.k = k;
+        for (int num : nums) {
+            if (queue.size() < k) {
+                queue.add(num);
+            } else {
+                if (queue.peek() < num) {
+                    queue.add(num);
+                    queue.poll();
+                }
+            }
+        }
+    }
+
+    public int add(int val) {
+        if (queue.size() < k) {
+            queue.add(val);
+        } else {
+            if (queue.peek() < val) {
+                queue.add(val);
+                queue.poll();
+            }
+        }
+        return queue.peek();
+    }
+}
